43c0408c01c15cdb867e1ce51b25ae0e2f2b53ef
Edu <edu_marzo_98@hotmail.com>
2025-02-11 07:37:32 +0000

    Repeat search bar to study

--- main.js
+++ main.js
@@ -1,38 +1,38 @@
 const url = "./assets/data/clothe.json"
-const clotheCardTemplate = document.querySelector("[clothe-card-template]")
-const searchInput = document.querySelector("[data-search]")
-const clotheCardsContainer = document.querySelector("[clothe-cards-container]")
+const clotheTemplate = document.querySelector('[clothe-card-template]')
+const clotheCardsContainer = document.querySelector('[clothe-cards-container]')
+const searchInput = document.querySelector('[data-search]')
 
 let clothesArray = []
 
 const detectInput = e => {
     const value = e.target.value.toLowerCase()
-    clothesArray.forEach(clothe => {
+    clothesArray.forEach(clothe =>{
         if(clothe.name.toLowerCase().includes(value)){
-            console.log(clothe.element)
             clothe.element.classList.remove('hide')
         }else{
             clothe.element.classList.add('hide')
         }
     })
-    console.log(value)
 }
 
+//Fetch
 fetch(url)
 .then(response => response.json())
 .then(data => {
+    //Recorrer el JSON
     clothesArray = data.map(element => {
-        const card = clotheCardTemplate.content.cloneNode(true).children[0];
-        const header = card.querySelector("[clothe-header]")
-        const body = card.querySelector("[clothe-body]")
-        const image = card.querySelector("[clothe-image]")
-        header.textContent = element.name
+        //Crear un Fragmento y añadir la info del JSON a los nodos hijos del fragmento
+        const card = clotheTemplate.content.cloneNode(true).children[0]
+        const image = card.querySelector('[clothe-image]')
         image.src = element.image
-        image.alt = element.name.toLowerCase()
-        body.textContent = element.price + ' '+element.coin
+        const header = card.querySelector('[clothe-header]')
+        header.textContent = element.name
+        const body = card.querySelector('[clothe-body]')
+        body.textContent = element.price
         clotheCardsContainer.append(card)
         return {name: element.name, price: element.price, image: element.image, element: card}
-    });
+    })
 })
 
-searchInput.addEventListener("input", detectInput)    
+searchInput.addEventListener('input', detectInput)
